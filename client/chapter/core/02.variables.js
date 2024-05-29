00c190d72261b05937735d66b45d5bc2d65e8e9f
kindtiger <seonbeom2@gmail.com>
2024-05-29 16:13:14 +0900

    learn. core javascript
    - code Structure
    - variables
    - staticMode
    - globalThis
    - legacyVar

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,52 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+console.log(admin);
+
+
+
+const OUR_PLANET_NAME = 'Earth';
+const currentOnlineUserName = 'tiger';
+
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// hasClass => t/f
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 구매 결제 여부
+// true / false => boolean data
+let isPayment = false;
+let hasCartList = false;
+
+// - 구매 결제 내역
+let paymentList;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'Stussy';
+
+// - 오늘의 운세
+let fortuneToday = 'so good';
+
+// - 상품 정보
+const product_Infomation = '';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
+// - 1년 기준 일(day)자 수 365
+const DAY_PER_YEAR = 365;
+
+
--- server/index.js
+++ server/index.js
@@ -1,17 +1,20 @@
 
 
 
-const server = require('live-server');
+// const server = require('live-server');
 
-const params = {
-  host: 'localhost',
-  port: 3000,
-  open: false,
-  root: './client'
-}
+// const params = {
+//   host: 'localhost',
+//   port: 3000,
+//   open: false,
+//   root: './client'
+// }
 
 
-server.start(params)
+// server.start(params)
+
+
+console.log(global)
 
 
 
