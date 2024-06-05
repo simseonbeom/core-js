2ad05271acd5fac18d4188b60b618c7e3441e905
kindtiger <seonbeom2@gmail.com>
2024-06-05 14:33:42 +0900

    learn. core javascript
    - 콜백함수 응용하기

--- client/chapter/core/11-2.function.js
+++ client/chapter/core/11-2.function.js
@@ -34,46 +34,140 @@ let calculateTotal = function (){
   //   total += value;
   // }
 
-  console.log(arguments);
+  // console.log(arguments);
 
 
   // arguments => array
 
-  // const arr = [...arguments]
+  const arr = [...arguments]
   // const arr = Array.from(arguments)
-  const arr = Array.prototype.slice.call(arguments);
+  // const arr = Array.prototype.slice.call(arguments);
            
-  console.log( arr );
+  // console.log( arr );
 
 
-  arr.forEach(function(price){
-    
-    total += price;
+  // forEach
+  // arr.forEach(function(price){
+  //   total += price;
+  // })
 
-  })
+  // arr.forEach(price => total += price)
+
+  // reduce
+  // const result = arr.reduce(function(acc,cur){
+  //   return acc + cur
+  // },0)
+
+  // const result = arr.reduce((acc,cur) => acc + cur,0)
+
+  // return arr.reduce((acc,cur) => acc + cur,0)
 
 
+  // 빌려쓰기
+  // Array.prototype.forEach.call(arguments,function(item){
+  //   total += item
+  // })
+
+  // 태생을 배열로 바꾸기
+  arguments.__proto__ = Array.prototype
   
-
   return total
 
 };
 
+// let calculateTotal2 = (arr) => arr.reduce((acc,cur) => acc + cur,0)
 
 const result = calculateTotal(1000,5000,2500,4000,2300);
 
-console.log( result );
+// console.log( result );
+
+
+// forEach => 배열 순환 값 반환 x 
+// reduce  => 배열 순환 값 반환 o  숫자/문자/배열/객체
+// map     => 배열 순환 값 반환 o  only 배열
+
+
+// filter  => 배열 순환 값 반환 o  only 배열
+
+
+
+const arr = ['이민제','안재명','함정민']
+
+// ['멋쟁이-이민제','멋쟁이-안재명','멋쟁이-함정민']
+
+const mapValue = arr.map(function(item){
+  return '멋쟁이-' + item
+})
+
+console.log( mapValue );
+
+
 
 // 익명(이름이 없는) 함수 (표현)식
-let anonymousFunctionExpression;
+let anonymousFunctionExpression = function(){};
 
 
 // 유명(이름을 가진) 함수 (표현)식
-let namedFunctionExpression;
+let namedFunctionExpression = function hello(){
+
+};
 
 
 // 콜백 함수 (표현)식
-let callbackFunctionExpression;
+let cb = function(isActive,success,fail){
+  if(isActive){
+    success()
+  }else{
+    fail()
+  }
+};
+
+
+cb(
+  false,
+  function(){
+    console.log('성공입니다!');
+  },
+  function(){
+    console.log('실패입니다..');
+  }
+)
+
+
+
+function movePage(url,success,fail){
+
+  if(url.includes('https')){
+    success(url)
+  }else{
+    fail()
+  }
+}
+
+
+movePage(
+  'https://www.daum.net',
+  function(url){
+ 
+    console.log(`현재 입력하신 url은 ${url}입니다. 3초 뒤 해당 사이트로 이동합니다.`);
+    location.href = url
+  },
+  function(){
+    console.log('잘못된 url을 입력하셨습니다.');
+  }
+)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // 함수 선언문 vs. 함수 (표현)식
@@ -82,3 +176,47 @@ let callbackFunctionExpression;
 // 즉시 실행 함수 (표현)식
 // Immediately Invoked Function Expression
 let IIFE;
+
+
+
+
+
+
+
+
+const people = [
+  {
+    nickName:'tiger',
+    age:40
+  },
+  {
+    nickName:'beom',
+    age:45
+  },
+  {
+    nickName: 'seon',
+    age:20
+  }
+]
+
+
+
+const template = people.reduce(function(htmlCode,cur){
+  return htmlCode + `<div>${cur.nickName} : ${cur.age}</div>`
+},'')
+
+
+
+// document.body.insertAdjacentHTML('beforeend',template)
+
+
+
+
+
+
+
+
+
+
+
+
