6c518c701eee4b98d01318b776d1a5a08a696c21
kindtiger <seonbeom2@gmail.com>
2024-06-25 11:37:52 +0900

    learn. randomDice
    - renderItem
    - createItem
    - endScroll
    - reset

--- client/lib/dom/index.js
+++ client/lib/dom/index.js
@@ -10,6 +10,7 @@ export * from './insert.js';
 export * from './getNode.js';
 export * from './showAlert.js';
 export * from './bindEvent.js';
+export * from './endScroll.js';
 export * from './clearContents.js';
 export {default as clearContents} from './clearContents.js';
 
--- client/main.js
+++ client/main.js
@@ -1,8 +1,13 @@
 
 
 import { 
+  attr,
+  getNode,
   getNodes,
+  endScroll,
+  insertLast,
   diceAnimation,
+  clearContents,
  } from "./lib/index.js";
 
 
@@ -13,9 +18,41 @@ import {
 
 
 const [rollingButton,recordButton,resetButton] = getNodes('.buttonGroup > button');
+const recordListWrapper = getNode('.recordListWrapper');
 
 
+let count = 0;
+let total = 0;
 
+
+function createItem(value){
+  const template = `
+    <tr>
+      <td>${++count}</td>
+      <td>${value}</td>
+      <td>${total += value}</td>
+    </tr>
+  `
+  return template
+}
+
+
+function renderRecordItem(){
+  
+  // const diceValue = getNode('#cube').getAttribute('dice');
+  const diceValue = Number(attr(getNode('#cube'),'dice'));
+
+
+  insertLast('.recordList tbody',createItem(diceValue))
+  
+  // 1. insertLast 함수 사용
+
+  // 2. template 전달
+
+  // 3. diceValue interpolation(보간법) 하기 ex) <td>${diceValue}</td>
+  endScroll(recordListWrapper);
+
+}
 
 
 
@@ -27,21 +64,38 @@ const handleRollingDice = (() => {
 
   return ()=>{
     if(!isClicked){
-      console.log('클릭 첫 번째');
       stopAnimation  = setInterval(diceAnimation, 100);
-  
+      recordButton.disabled = true;
+      resetButton.disabled = true;
     }
     else{
-      console.log('클릭 두 번째');
-      clearInterval(stopAnimation)
+      clearInterval(stopAnimation);
+      recordButton.disabled = false;
+      resetButton.disabled = false;
     }
     isClicked = !isClicked;
   }
 })()
 
 
+function handleRecord(){
+  recordListWrapper.hidden = false;
+
+  renderRecordItem()
+}
+
+
+function handleReset(){
+  recordListWrapper.hidden = true;
+  clearContents('tbody');
+  count = 0;
+  total = 0;
+  
+}
 
 rollingButton.addEventListener('click',handleRollingDice)
+recordButton.addEventListener('click',handleRecord)
+resetButton.addEventListener('click',handleReset)
 
 
 
