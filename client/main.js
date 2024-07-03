c091cfe1914a9a79a46fc0d73d44eac5186da06c
kindtiger <seonbeom2@gmail.com>
2024-07-03 11:51:25 +0900

    learn. webComponent
    - slot

--- client/main.js
+++ client/main.js
@@ -1,28 +1,28 @@
 
 
 
-const app = document.querySelector('#app');
-const temp = document.querySelector('#temp');
 
 
+class UserCard extends HTMLElement{
+  constructor(){
+    super();
+    this.attachShadow({mode:'open'});
+    this.shadowRoot.innerHTML = `
+        <div> nickName : kind-tiger </div>
+        <slot name="username"></slot>
+        <slot name="age"></slot>
+        <slot name="gender"></slot>
+        <slot name="common"></slot>
+        <slot></slot>
 
 
-const clone = temp.content.cloneNode(true);
+    `
 
-app.appendChild(clone);
-
-
-
+  }
+}
 
 
-
-
-
-
-
-
-
-
+customElements.define('user-card',UserCard);
 
 
 
