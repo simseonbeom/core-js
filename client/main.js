f2c4c6f5fc3502d03772079f60c22cdfa1e6af51
kindtiger <seonbeom2@gmail.com>
2024-07-03 11:38:51 +0900

    learn. webComponent
    - template

--- client/main.js
+++ client/main.js
@@ -1,47 +1,15 @@
 
 
 
-
-
-class Button extends HTMLElement {
-
-  constructor(){
-    super();
-
-    // c-button의 쉐도우 돔을 열어줘.
-    this.attachShadow({mode:'open'});
-
-    // 그리고 그 안에(shadowRoot) 내가 원하는 태그 집어 넣을거야 
-    this.shadowRoot.innerHTML = `
-      <button>hello</button>
-    `
-  }
-
-  connectedCallback(){
-
-  }
-
-  disconnectedCallback(){
-
-  }
-
-}
-
-
-
-customElements.define('c-button',Button);
-
-
-console.log( document.querySelector('c-button').shadowRoot.querySelector('button') );
+const app = document.querySelector('#app');
+const temp = document.querySelector('#temp');
 
 
 
 
+const clone = temp.content.cloneNode(true);
 
-
-
-
-
+app.appendChild(clone);
 
 
 
