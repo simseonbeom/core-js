c985ccb5d9a2ebddcb13f68d03af931ddec6f597
kindtiger <seonbeom2@gmail.com>
2024-07-03 11:14:53 +0900

    learn. webComponent
    - shadowDOM

--- client/main.js
+++ client/main.js
@@ -3,10 +3,18 @@
 
 
 
-class Button extends HTMLButtonElement {
+class Button extends HTMLElement {
 
   constructor(){
     super();
+
+    // c-button의 쉐도우 돔을 열어줘.
+    this.attachShadow({mode:'open'});
+
+    // 그리고 그 안에(shadowRoot) 내가 원하는 태그 집어 넣을거야 
+    this.shadowRoot.innerHTML = `
+      <button>hello</button>
+    `
   }
 
   connectedCallback(){
@@ -20,9 +28,11 @@ class Button extends HTMLButtonElement {
 }
 
 
-customElements.define('c-button',Button,{extends:'button'});
+
+customElements.define('c-button',Button);
 
 
+console.log( document.querySelector('c-button').shadowRoot.querySelector('button') );
 
 
 
