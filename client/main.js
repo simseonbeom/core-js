ba4e69b55aca148051a9c6b03c3d787c21c5e267
kindtiger <seonbeom2@gmail.com>
2024-07-03 13:26:52 +0900

    learn. webComponent
    - DOM style

--- client/main.js
+++ client/main.js
@@ -8,12 +8,21 @@ class UserCard extends HTMLElement{
     super();
     this.attachShadow({mode:'open'});
     this.shadowRoot.innerHTML = `
+      <style>
+        :host{
+          background-color: orange;
+        }
+        div{
+          background:red;
+        }
+      </style>
         <div> nickName : kind-tiger </div>
         <slot name="username"></slot>
         <slot name="age"></slot>
         <slot name="gender"></slot>
         <slot name="common"></slot>
         <slot></slot>
+        <slot name="email">any@naver.com</slot>
 
 
     `
