b96c736d16043b932b54589339ae63956c5e1e5d
kindtiger <seonbeom2@gmail.com>
2024-07-03 14:50:10 +0900

    learn. webComponent
    - 환경설정

--- client/main.js
+++ client/main.js
@@ -3,62 +3,6 @@
 
 
 
-class UserCard extends HTMLElement{
-  constructor(){
-    super();
-    this.attachShadow({mode:'open'});
-    this.shadowRoot.innerHTML = `
-      <button type="button">btn</button>
-    `
-    this.button = this.shadowRoot.querySelector('button');
-  }
-
-  connectedCallback(){
-
-    this.button.addEventListener('click',()=> this.clickMe() )
-
-  }
-
-  
-
-  clickMe(){
-    console.log(this);
-  }
-
-
-
-}
-
-
-customElements.define('user-card',UserCard);
-
-
-
-// console.log( document.querySelector('user-card').shadowRoot.querySelector('button') );
-
-
-
-
-// function sum(){
-//   console.log(this);
-// }
-
-
-// sum() // undefined
-// sum.call({}) // {} => 실행
-// sum.apply({}) // {} => 실행
-// const n = sum.bind({}) // {} => 실행 x 
-
-// n()
-
-
-
-
-
-
-
-
-
 
 
 
