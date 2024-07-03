b2d94f060c1793c5a7c8997e19b2815918066f06
kindtiger <seonbeom2@gmail.com>
2024-07-03 10:36:47 +0900

    learn. webComponent
    - customized built-in element

--- client/main.js
+++ client/main.js
@@ -3,51 +3,47 @@
 
 
 
-class Button extends HTMLElement {
+class Button extends HTMLButtonElement {
 
   constructor(){
     super();
-
-    this.button = document.querySelector('button');
-
   }
 
   connectedCallback(){
-    this._render();
+
   }
 
   disconnectedCallback(){
 
   }
 
-  static get observedAttributes(){
-    return ['id'];
-  }
-
-  attributeChangedCallback(name,oldValue,newValue){
-    if(oldValue !== newValue){
-      this._render()
-    }
-  }
-
-  _render(){
-    this.button.textContent = this.id;
-  }
-
 }
 
 
-customElements.define('c-button',Button);
+customElements.define('c-button',Button,{extends:'button'});
 
 
 
-const c = document.querySelector('c-button');
 
-let count = 0;
 
-c.addEventListener('click',()=>{
-  c.setAttribute('id',++count)
-})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
